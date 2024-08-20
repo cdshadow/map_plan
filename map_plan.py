4a5bd2773a746aee0848a7baa8d7fa16097ec426
cdshadow <153569166+cdshadow@users.noreply.github.com>
2024-08-20 09:49:08 +0900

    Update map_plan.py

--- map_plan.py
+++ map_plan.py
@@ -9,6 +9,8 @@ file_path = 'data.csv'
 
 # Streamlit 설정
 #st.set_page_config(layout="wide")
+#st.set_page_config(layout="centered")
+
 
 # 데이터 파일 경로
 shp_file_path_1f = 'https://raw.githubusercontent.com/cdshadow/map_plan/main/1f_2.shp'
